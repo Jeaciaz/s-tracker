6b10084104d3f0aa4fb222d73e94e3695431012e
Jeaciaz <jeaciaz@pop-os.localdomain>
2023-06-14 11:59:50 +0300

    Add CORS for localhost development

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import funnels, spendings
 from .database import metadata_obj, engine
+
+allowed_origins = [
+    "http://localhost:3000"
+]
 
 def make_app() -> FastAPI:
     app = FastAPI()
 
     app.include_router(funnels.router)
     app.include_router(spendings.router)
+
+    app.add_middleware(CORSMiddleware, allow_origins=allowed_origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
     return app
 
