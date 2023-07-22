721b1c47c66303b3ec0b503ecb362c9872b10e21
Jeaciaz <jeaciaz@pop-os.localdomain>
2023-07-22 16:15:55 +0300

    fix wrong backend port

--- frontend/src/xtracker/config.cljs
+++ frontend/src/xtracker/config.cljs
@@ -3,4 +3,4 @@
 (def funnel-total-visuals {:name "Всего"
                            :color "#884EA0"})
 
-(def base-url (str (. js/window.location -protocol) "//" (. js/window.location -hostname) ":8000"))
+(def base-url (str (. js/window.location -protocol) "//" (. js/window.location -hostname) ":8080"))
